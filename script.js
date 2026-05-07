2b3d57b274846fc8d8b1fe6821bc947b1b229364
trevortamp <trevor.tamp@techno.ee>
2026-05-07 10:36:01 +0300

    3

--- script.js
+++
@@ -1,7 +0,0 @@
-const response = await fetch('https://tinkr.tech/sdb/a', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ name: 'John', age: 25 })
-});
-const data = await response.json();
-console.log(data); 
